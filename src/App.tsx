a07333f98c9f4b49df4f3b4ca6a40e7befa5d4fe
rodi-ugurlu <rodiiugurlu@gmail.com>
2026-08-11 15:31:24 +0300

    fix: configure global Ant Design trTR locale in App.tsx for Turkish sorter tooltips and UI components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { ConfigProvider } from 'antd';
+import trTR from 'antd/locale/tr_TR';
 import { AppRouter } from './router/AppRouter';
 
 function App() {
-  return <AppRouter />;
+  return (
+    <ConfigProvider locale={trTR}>
+      <AppRouter />
+    </ConfigProvider>
+  );
 }
 
 export default App;
